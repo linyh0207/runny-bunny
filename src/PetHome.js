8311040ec35339c76cab17c73099961b4566dac0
Mandy Fung <mandyfung93@gmail.com>
2019-02-09 19:01:03 -0800

    ma/initial yellow box

--- src/PetHome.js
+++ src/PetHome.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import { 
+import {
+  LayoutAnimation,
+  NativeModules, 
   StyleSheet, 
-  Text, 
+  Text,
+  TouchableOpacity, 
   View 
 } from 'react-native';
 
+const { UIManager} = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
+
 class PetHome extends React.Component {
   static navigationOptions = {
-    title: 'Pet Home'
+    title: 'Pet Home',
   };
+  constructor(props){
+    super(props);
+    this.state = {
+      w: 100,
+      h: 100,
+    }
+  }
+  onPress = () => {
+    // Animate the update
+    LayoutAnimation.spring();
+    this.setState({w: this.state.w + 15, h: this.state.h + 15})
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>This is Pet Home Page!</Text>
-      </View>
+        <View style={[styles.box, {width: this.state.w, height: this.state.h}]} />
+          <TouchableOpacity onPress={this.onPress}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Press me!</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
     );
   }
 }
@@ -26,6 +53,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'yellow'
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
 
 export default PetHome;
